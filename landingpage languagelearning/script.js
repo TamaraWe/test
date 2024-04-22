09a6537fcb2ed09b0dbd3f1f437a389067d8735b
TamaraWe <37803573+TamaraWe@users.noreply.github.com>
2024-04-22 22:07:23 +0200

    Update script.js

--- landingpage languagelearning/script.js
+++ landingpage languagelearning/script.js
@@ -1,4 +1,4 @@
-
+// Navigation Scroll-Change
 const headerEl = document.querySelector('.nav-container');
 const menutrigger = document.querySelector('.navigation');
 const body = document.querySelector('body');
@@ -11,7 +11,18 @@ window.addEventListener('scroll', function(event) {
  }
 });
 
+// Mobile Navigation Menu Toggle
 function menutoggle() {
   menutrigger.classList.toggle('mobile-menu-closed');
   body.classList.toggle('no-scroll');
 }
+
+// Testimonial Carousel
+let testimonials = document.querySelectorAll('.testimonal');
+let thumbnails = document.querySelectorAll('.testimonial-thumbs img');
+
+function showTestimonial(index) {
+  testimonials.forEach(testimonial => testimonial.style.display = 'none');
+  testimonials[index].style.display = 'flex';
+}
+showTestimonial(0);
