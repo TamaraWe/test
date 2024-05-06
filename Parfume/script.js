9aaefac97248ac840f390975705f194c4606a0c2
TamaraWe <37803573+TamaraWe@users.noreply.github.com>
2024-05-06 22:23:50 +0200

    Add files via upload

---
+++ Parfume/script.js
@@ -0,0 +1,17 @@
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  const accordionHeaders = document.querySelectorAll('.accordion-header');
+
+  accordionHeaders.forEach(header => {
+    header.addEventListener('click', function() {
+      this.classList.toggle('active');
+      const content = this.nextElementSibling;
+      if (content.style.display === 'block') {
+        content.style.display = 'none';
+      } else {
+        content.style.display = 'block';
+      }
+    });
+  });
+});
